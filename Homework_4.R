40e4983e8e07ca1a5d3f65a99fef641380c53a32
beisenpress <ben@eisenpress.com>
2016-04-30 15:01:55 -0500

    Initial data and files

---
+++ Homework_4.R
@@ -0,0 +1,9 @@
+#### 37301 Data Driven Marketing ######
+#### Homework 4 #######
+
+library(psych)
+library(lattice)
+library(plyr)
+
+setwd("/Users/ben/Dropbox/Chicago Booth/37103 Data Driven Marketing/Homework 4/37103-HW4")
+load("Slot-Data.RData")
